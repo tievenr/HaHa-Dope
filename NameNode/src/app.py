328977dc0d98ae4ff0e182ba225f9bf71c440753
tievenr <vineetraghu17@gmail.com>
2025-07-28 17:50:48 +0530

    feat: setup a post enpoint in namenode that requests the client for filename and size

--- NameNode/src/app.py
+++ NameNode/src/app.py
@@ -1,24 +1,47 @@
 import logging
 from fastapi import FastAPI, Request
+from block_manager import split_file_into_blocks
+from pydantic import BaseModel
 
+## Logger setup
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s: %(name)s: %(levelname)s: %(message)s',
     handlers=[
-        logging.FileHandler('/usr/local/app/namenode_logs/namenode.log'),  #This line is for persistent logs under the log volume
-        logging.StreamHandler()  # This one is for when docker container logs
+        logging.FileHandler('/usr/local/app/namenode_logs/namenode.log'),  
+        logging.StreamHandler()
     ]
 )
 logger = logging.getLogger(__name__)
+# Logger done initialized above 🥀
+
 
 app = FastAPI()
 
+
+#pydantic class for file upload
+class FileUploadRequest(BaseModel):
+    filename:str
+    filesize_bytes:int
+
+#below function be beating only if Namenode is up and running 
 @app.get("/health")
 async def namenode_healthcheck():
     return {"status": "ok"}
 
+
+# ts is to just for namenode to know that datanode is alive
 @app.post("/nodes/{node_id}/heartbeat")
 async def recieve_heartbeats(node_id: str, request: Request):
     payload = await request.json()
     logger.info(f"Heartbeat received from {node_id}: {payload}")
     return {"recieved from": node_id, "payload": payload}
+
+# This one is when client upload the file so namenode has to split it up 
+@app.post("/files")
+async def upload_file(file_request: FileUploadRequest):
+    filename= file_request.filename
+    filesize_bytes = file_request.filesize_bytes
+    blocks = split_file_into_blocks(filename, filesize_bytes)
+
+    return {"message": "File processed", "blocks": blocks}
