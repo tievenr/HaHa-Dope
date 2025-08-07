46c201ea4af7fd50a5ce7a46f5fcaab1b5b599fa
tievenr <vineetraghu17@gmail.com>
2025-08-07 17:10:28 +0530

    feat: set up namenode health check in the client

--- client.py
+++ client.py
@@ -1,3 +1,21 @@
 import os
 import requests
 import json
+
+namenode_url = "http://127.0.0.1:9870"
+
+def namenode_healthcheck():
+
+    try: 
+        res = requests.get(namenode_url +"/health")
+        return res.status_code == 200
+    
+    except: 
+        return False
+    
+if __name__ == "__main__":
+    print("Testing NameNode connection...")
+    if namenode_healthcheck():
+        print("NameNode is healthy!")
+    else:
+        print("NameNode is not reachable")
