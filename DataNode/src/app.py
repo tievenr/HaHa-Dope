2f0dffce73d52844e7d5a7b72fddea4a10dde76e
tievenr <vineetraghu17@gmail.com>
2025-08-10 16:22:36 +0530

    fix: set up an lifespan handler to run heartbeats on a background thread when the fastapi server starts and initialized a datanodes health point

--- DataNode/src/app.py
+++ DataNode/src/app.py
@@ -1,8 +1,12 @@
+from fastapi import FastAPI
 import requests
 import os
 import logging
 import time
 from datetime import datetime, timezone
+import threading
+from contextlib import asynccontextmanager
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -10,15 +14,42 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-node_id = os.getenv("NODE_ID", "datanode")
-heartbeat_url = f"http://namenode:9870/nodes/{node_id}/heartbeat"
+NODE_ID = os.getenv("NODE_ID", "datanode")
+DATA_DIR = f"/usr/local/app/data/{NODE_ID}"
+os.makedirs(DATA_DIR, exist_ok=True)
+HEARTBEAT_URL = f"http://namenode:9870/nodes/{NODE_ID}/heartbeat"
 
-while True:
-    timestamp = datetime.now(timezone.utc)
-    payload = {"node_id": node_id, "timestamp": timestamp.isoformat()}
-    try:
-        response = requests.post(heartbeat_url, json=payload)
-        logger.info(f"{timestamp} | {response.status_code} | {response.json()}")
-    except Exception as e:
-        logger.error(f"{timestamp} | Error: {e}")
-    time.sleep(1)
+
+def send_heartbeats():
+    """Background function to send heartbeats"""
+    while True:
+        timestamp = datetime.now(timezone.utc)
+        payload = {"node_id": NODE_ID, "timestamp": timestamp.isoformat()}
+        try:
+            response = requests.post(HEARTBEAT_URL, json=payload)
+            logger.info(f"{timestamp} | {response.status_code} | {response.json()}")
+        except Exception as e:
+            logger.error(f"{timestamp} | Error: {e}")
+        time.sleep(1)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    thread = threading.Thread(target=send_heartbeats, daemon=True)
+    thread.start()
+    logger.info(f"DataNode {NODE_ID} started, sending heartbeats to NameNode")
+    yield
+
+app = FastAPI(title=f"DataNode {NODE_ID}", lifespan=lifespan)
+
+
+# Health enpoint for datanode
+@app.get("/health")
+async def datanode_health():
+    return {"status": "ok", "node_id": NODE_ID}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
